1fea18539b369e645415f61ff3fbca1baf65da14
ammmarr <ammarrelkosii@yahoo.com>
2022-11-01 15:43:53 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- build: { outDir: "docs" },
+ build: { outDir: "dist" },
   base: "/Ammar-s-protofolio/",
 });
